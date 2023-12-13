3561348c81b8d8269b9805df0c14aadcf88b4bb0
Nebur23 <yvesnyemb7@gmail.com>
2023-12-13 18:23:51 +0100

    fixed deploy errors

--- action/get-billboards.ts
+++
@@ -1,6 +0,0 @@
-export default async  function getAllBillboards(id: string ) {
-  const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards/${id}` as string
-  const res = await fetch(URL);
- 
-  return await res.json()
-}
--- action/get-categories.ts
+++
@@ -1,8 +0,0 @@
-import { Category } from "@/types";
-
-export default async  function getAllCategories(): Promise<Category[]> {
-    const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories` as string
-    const res = await fetch(URL);
-   
-    return await res.json()
-  }
